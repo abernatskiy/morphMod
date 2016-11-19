1848c95965cba3ee4fbfecab88958afd81a8d3a9
Anton Bernatskiy (cluster) <abernats@uvm.edu>
2016-11-18 20:31:41 -0500

    Fixed some nasty typos and updated pbsGriWalker ver
    	modified:   pbsGridWalker
    	modified:   twoMorphologies.py

--- twoMorphologies.py
+++ twoMorphologies.py
@@ -17,13 +17,14 @@ evsClassifier = {'classes': ['individual', 'communicator', 'evolver'],
              'indivParams': ['length', 'mutProbability', 'mutInsDelRatio', 'mutExploration',
                              'initLowerLimit', 'initUpperLimit', 'lowerCap', 'upperCap',
                              'initProbabilityOfConnection', 'mutationAmplitude'],
-              'evolParams': ['populationSize', 'genStopAfter', 'initialPopulationType', 'trackAncestry'
+              'evolParams': ['populationSize', 'genStopAfter', 'initialPopulationType', 'trackAncestry',
                              'secondObjectiveProbability', 'logPopulation', 'logPopulationPeriod',
                              'logBestIndividual', 'logBestIndividualPeriod', 'printBestIndividual',
                              'printBestIndividualPeriod', 'printParetoFront', 'printParetoFrontPeriod',
                              'printPopulation', 'printPopulationPeriod', 'printGeneration',
                              'printGenerationPeriod', 'backup', 'backupPeriod', 'logParetoFront',
-                             'logParetoFrontPeriod', 'logParetoFrontKeepAllGenerations']
+                             'logParetoFrontPeriod', 'logParetoFrontKeepAllGenerations',
+                             'randomSeed']
              }
 arrowbotsClassifier = {'arrowbot parameters': ['segments', 'sensorAttachmentType'],
                     'simulation parameters': ['simulationTime', 'timeStep', 'integrateError', 'writeTrajectories']
