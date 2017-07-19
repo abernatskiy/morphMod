8de955070de868ddcf2269cd14a1c3d428989387
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-07-19 20:14:42 +0900

    New regexp-based classifiers added to deal with composites
    	modified:   classifiers.py
    	modified:   pbsGridWalker

--- classifiers.py
+++ classifiers.py
@@ -1,9 +1,15 @@
 serverClientClassifier = {'server': ['randomSeed', 'initialPopulationType'], 'client': ['sensorAttachmentType']}
 
+# Since EVS has supported composite Individuals, it now requires a regexp classifier
+_indivClassParams = ['length', 'mutProbability', 'mutInsDelRatio', 'mutExploration',
+                     'initLowerLimit', 'initUpperLimit', 'lowerCap', 'upperCap',
+                     'initProbabilityOfConnection', 'mutationAmplitude']
+_indivClassParamsRegexp = '(' + '|'.join(_indivClassParams) + ')'
+_classSuffixRegexp = 'Class[0-9]+'
 evsClassifier = {'classes': ['individual', 'communicator', 'evolver'],
-             'indivParams': ['length', 'mutProbability', 'mutInsDelRatio', 'mutExploration',
-                             'initLowerLimit', 'initUpperLimit', 'lowerCap', 'upperCap',
-                             'initProbabilityOfConnection', 'mutationAmplitude'],
+             'indivParams': [ _indivClassParamsRegexp,
+                              _indivClassParamsRegexp + _classSuffixRegexp,
+                              '(composite|probabilityOfMutating)' + _classSuffixRegexp ],
               'evolParams': ['populationSize', 'genStopAfter', 'initialPopulationType', 'trackAncestry',
                              'secondObjectiveProbability', 'logPopulation', 'logPopulationPeriod',
                              'logBestIndividual', 'logBestIndividualPeriod', 'printBestIndividual',
@@ -11,7 +17,7 @@ evsClassifier = {'classes': ['individual', 'communicator', 'evolver'],
                              'printPopulation', 'printPopulationPeriod', 'printGeneration',
                              'printGenerationPeriod', 'backup', 'backupPeriod', 'logParetoFront',
                              'logParetoFrontPeriod', 'logParetoFrontKeepAllGenerations',
-                             'randomSeed']}
+                             'randomSeed', 'morphologyControlIndivs']}
 
 arrowbotsClassifier = {'arrowbot parameters': ['segments', 'sensorAttachmentType'],
                     'simulation parameters': ['simulationTime', 'timeStep', 'integrateError', 'writeTrajectories']}
