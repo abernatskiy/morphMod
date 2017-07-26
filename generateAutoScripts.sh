fcac78f5978d074164e50488e99c06fd6ebcfdc6
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-07-26 23:10:07 +0900

    Switching to the old convention for autoscripts name
    The new one was unbearably annoying.
    
    	modified:   generateAutoScripts.sh

--- generateAutoScripts.sh
+++ generateAutoScripts.sh
@@ -6,7 +6,7 @@ generateOneScript() {
 	local base_script="${1}"
 	local num_segments="${2}"
 	local stop_after_generations="${3}"
-	cat "${base_script}.py" | sed -e "s/segments = [0-9]*/segments = ${num_segments}/" | sed -e "s/'genStopAfter': [0-9]*/'genStopAfter': ${stop_after_generations}/" > "autoscripts/${base_script}_${num_segments}segments.py"
+	cat "${base_script}.py" | sed -e "s/segments = [0-9]*/segments = ${num_segments}/" | sed -e "s/'genStopAfter': [0-9]*/'genStopAfter': ${stop_after_generations}/" > "autoscripts/${base_script}_N${num_segments}.py"
 }
 
 generateFromOneBase() {
