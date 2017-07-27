68f6a44843d2ee39e87ebbb9c7c7ea01c608e297
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-07-28 01:54:14 +0900

    Corrected the rateSwipe params + fixes to evs and pgw
    	modified:   evs
    	modified:   pbsGridWalker
    	modified:   rateSwipe.py

--- rateSwipe.py
+++ rateSwipe.py
@@ -18,7 +18,7 @@ import gctools
 #dryRun = False
 
 # Tunable hyperparameters
-numTrials = 20
+numTrials = 100
 segments = 3
 computationName = 'rateSwipe_N' + str(segments)
 
@@ -30,7 +30,7 @@ evsAdditionalParams = {'individual': 'compositeFixedProbabilities', 'evolver': '
                        'compositeClass1': 'integerWeightsSwitchableConnections',
                        'lengthClass1': 2*(segments**2), 'initLowerLimitClass1': -1, 'initUpperLimitClass1': 1, 'lowerCapClass1': -1, 'upperCapClass1': 1,
                        'mutExplorationClass1': 0.8, 'mutInsDelRatioClass1': 1, 'mutationAmplitudeClass1': 1,
-                       'genStopAfter': 200, 'populationSize': 50, 'secondObjectiveProbability': 1.,
+                       'genStopAfter': 200, 'populationSize': 25, 'secondObjectiveProbability': 1.,
                        'logParetoFront': 'yes', 'logBestIndividual': 'yes', 'logParetoFrontKeepAllGenerations': 'yes', 'logParetoFrontPeriod': 1,
                        'backup': 'no', 'trackAncestry': 'no'}
 arrowbotsAdditionalParams = {'segments': segments, 'sensorAttachmentType': 'variable',
@@ -39,7 +39,7 @@ arrowbotInitialConditions = [[0]*segments]*segments # segmentsXsegments null mat
 arrowbotTargetOrientations = [ [1 if i==j else 0 for i in range(segments)] for j in range(segments) ] # segmentsXsegments identity matrix
 
 # Optional definitions for pbsGridWalker that depend on the number of segments
-pointsPerJob = 1
+pointsPerJob = 20
 queue = 'shortq'
 expectedWallClockTime = '01:00:00'
 
@@ -48,7 +48,7 @@ maxJobs = 2
 involvedGitRepositories = mmr.involvedGitRepositories
 
 # Required pbsGridWalker definitions
-_morphologicalMutationProbabilityGrid = gr.LinGrid('probabilityOfMutatingClass0', 0.1, 0.1, 0, 8)
+_morphologicalMutationProbabilityGrid = gr.LinGrid('probabilityOfMutatingClass0', 0.0, 0.1, 0, 10)
 _initialPopulationTypeGrid = gr.Grid1d('initialPopulationType', ['sparse', 'random'])
 parametricGrid = _morphologicalMutationProbabilityGrid * \
                  _initialPopulationTypeGrid * \
@@ -92,8 +92,17 @@ def processResults(experiment):
 	tfs.makeDirCarefully('results', maxBackups=100)
 
 	# We'll take a look at some parameters vs relative mutation rate at several stages (generation counts) along the evolutionary process
-	stagesToConsider = 5
-	stages = tal.splitIntegerRangeIntoStages(1, evsAdditionalParams['genStopAfter'], stagesToConsider)
+
+	# Linear stages
+#	stagesToConsider = 5
+#	stages = tal.splitIntegerRangeIntoStages(0, evsAdditionalParams['genStopAfter'], stagesToConsider)
+
+	# Exponential stages
+	stages = [0, 1]
+	mult = 5
+	while stages[-1] < evsAdditionalParams['genStopAfter']:
+		stages.append(stages[-1]*mult)
+	stages.pop()
 
 	##### Extracting and plotting the distance to the maximally modular morphology (MMM) for various values relative mutation rate #####
 	# mmmmdist and similar abbreviations stand for "minimal distance to the maximally modular morphology" (across the Pareto front)
@@ -165,17 +174,14 @@ def processResults(experiment):
 					fn = fitnessFileName({'initialPopulationType': ipt, 'probabilityOfMutatingClass0': mmprob}, gen)
 					mmmmddata[ipt][str(gen)].append(np.loadtxt(fn))
 					#os.remove(fn)
-				mmmmddata[ipt][str(gen)] = np.stack(mmmmddata[ipt][str(gen)]).T
-
-		mmmmddata['sparse'].pop('1')
-		mmmmddata['random'].pop('1')
+				mmmmddata[ipt][str(gen)] = -1.*np.stack(mmmmddata[ipt][str(gen)]).T
 
 		def plotFitnessVSMorphMod(initPopType):
 			filename = 'fitness_vs_mmrate_IP{}.png'.format(initPopType)
 			title = 'Fitness vs morphological mutation rate ({} initial population)'.format(initPopType)
 			margins = 0.5
 
-			tplt.plotAverageTimeSeries(mmmmddata[initPopType], 'fitness', filename, title=title, legendLocation=1, xlabel='mmrate', xlimit=1, ylimit=None, margins=margins, timeRange=mmprobslist)
+			tplt.plotAverageTimeSeries(mmmmddata[initPopType], 'error', filename, title=title, legendLocation=1, xlabel='mmrate', xlimit=1, ylimit=None, margins=margins, timeRange=mmprobslist, yscale='log')
 
 		map(plotFitnessVSMorphMod, iptypeslist)
 		#print(str(mmmmddata))
