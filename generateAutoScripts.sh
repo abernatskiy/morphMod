1b785ed2dbf2f88924173d3eab174ad3de5e5a53
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2017-07-30 00:54:39 +0900

    Simulation time is now tunable in autoscripts + pgw update
    Turns out larger arrowbots need more time to do the moving. Who
    could've guessed!
    
    Plus, PGW now has progress indication.
    
    	modified:   generateAutoScripts.sh
    	modified:   pbsGridWalker

--- generateAutoScripts.sh
+++ generateAutoScripts.sh
@@ -10,22 +10,26 @@ generateOneScript() {
 	local queue="${5}"
 	local requested_time="${6}"
 	local max_jobs="${7}"
+	local simulation_time="${8}"
+	local simulation_time_step="${9}"
 	cat "${base_script}.py" | sed \
 		-e "s/segments = [0-9]*/segments = ${num_segments}/" \
 		-e "s/'genStopAfter': [0-9]*/'genStopAfter': ${stop_after_generations}/" \
 		-e "s/'logParetoFrontPeriod': [0-9]*/'logParetoFrontPeriod': ${pareto_logging_period}/" \
 		-e "s/queue = '[a-z]*'/queue = '${queue}'/" \
 		-e "s/maxJobs = [0-9]*/maxJobs = ${max_jobs}/" \
-		-e "s/expectedWallClockTime = '.*'/expectedWallClockTime = '${requested_time}'/" > "autoscripts/${base_script}_N${num_segments}.py"
+		-e "s/expectedWallClockTime = '.*'/expectedWallClockTime = '${requested_time}'/" \
+		-e "s/'simulationTime': [\.0-9]*/'simulationTime': ${simulation_time}/" \
+		-e "s/'timeStep': [\.0-9]*/'timeStep': ${simulation_time_step}/" > "autoscripts/${base_script}_N${num_segments}.py"
 }
 
 generateFromOneBase() {
 	local base_script="${1}"
 	# Regarding convergence times: about 100 gens for 3 segments at probability of morphological mutation of 0.2, 400 for 5, 10 gets stuck indefinitely.
 	# The times are doubled where known and for 10 segments a placeholder 2000 is used for now
-	generateOneScript "${base_script}" 3 125 1 'shortq' '03:00:00' 2
-	generateOneScript "${base_script}" 5 625 5 'shortq' '03:00:00' 8
-	generateOneScript "${base_script}" 10 2000 10 'workq' '30:00:00' 100
+	generateOneScript "${base_script}" 3 125 1 'shortq' '03:00:00' 2 '3.0' '0.05'
+	generateOneScript "${base_script}" 5 625 5 'shortq' '03:00:00' 8 '3.0' '0.05'
+	generateOneScript "${base_script}" 10 1000 10 'workq' '30:00:00' 100 '6.0' '0.1'
 }
 
 generateFromOneBase simpleTimeSeries
