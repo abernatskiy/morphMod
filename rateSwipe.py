28bbf6382c2f666301e63bd548aa22631d372358
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2017-07-29 04:21:01 -0400

    rateSwipe now checks for random walk mutations
    	modified:   rateSwipe.py

--- rateSwipe.py
+++ rateSwipe.py
@@ -49,9 +49,11 @@ computationName = 'rateSwipe_N' + str(segments)
 
 evsDefaults.pop('probabilityOfMutatingClass0')
 evsDefaults.pop('initialPopulationType')
+evsDefaults.pop('compositeClass0')
 evsDefaults.pop('randomSeed')
 nonRSGrid = gr.LinGrid('probabilityOfMutatingClass0', 0.0, 0.05, 0, 20) * \
-            gr.Grid1d('initialPopulationType', ['sparse', 'random'])
+            gr.Grid1d('initialPopulationType', ['sparse', 'random']) * \
+            gr.Grid1d('compositeClass0', ['integerVectorSymmetricRangeMutations', 'integerVectorRandomJumps'])
 parametricGrid = nonRSGrid*numTrials + gr.Grid1dFromFile('randomSeed', mmr.randSeedFile, size=len(nonRSGrid)*numTrials)
 
 def prepareEnvironment(experiment):
