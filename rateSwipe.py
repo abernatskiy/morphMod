553d5cbf30472c013a54e28a60891bd42312efb8
Anton Bernatskiy (cluster) <abernats@uvm.edu>
2017-08-08 12:44:21 -0400

    Removed sparse initial populations from rateSwipe
    Because it is not very interesting
    
    	modified:   rateSwipe.py

--- rateSwipe.py
+++ rateSwipe.py
@@ -52,7 +52,6 @@ evsDefaults.pop('initialPopulationType')
 evsDefaults.pop('compositeClass0')
 evsDefaults.pop('randomSeed')
 nonRSGrid = gr.LinGrid('probabilityOfMutatingClass0', 0.0, 0.05, 0, 20) * \
-            gr.Grid1d('initialPopulationType', ['sparse', 'random']) * \
             gr.Grid1d('compositeClass0', ['integerVectorSymmetricRangeMutations', 'integerVectorRandomJumps'])
 parametricGrid = nonRSGrid*numTrials + gr.Grid1dFromFile('randomSeed', mmr.randSeedFile, size=len(nonRSGrid)*numTrials)
 
